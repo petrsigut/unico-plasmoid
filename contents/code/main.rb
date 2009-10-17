7f795d26d410e61c9fee26446f06f0f659df9aa7
Petr Sigut <petr@sigut.net>
2009-10-17 15:28:14 +0200

    konecne fachci dispose

--- contents/code/main.rb
+++ contents/code/main.rb
@@ -14,9 +14,9 @@ module RubyWidget
   # PlasmaScripting::Applet - for GHNS
   class Main < PlasmaScripting::Applet
 # The both slots used by our applet.
-    slots 'load(QUrl)',
-          'loadFinished(bool)',
-          :paserxml
+#    slots 'load(QUrl)',
+#          'loadFinished(bool)',
+#          :paserxml
 #    slots :add_x
 
     def initialize parent
@@ -24,94 +24,111 @@ module RubyWidget
     end
  
     def init
+      #Qt.debug_level = Qt::DebugLevel::High
       self.has_configuration_interface = true
       self.aspect_ratio_mode = Plasma::Square
       self.background_hints = Plasma::Applet.DefaultBackground
  
-      layout = Qt::GraphicsLinearLayout.new Qt::Vertical, self
+      @layout = Qt::GraphicsLinearLayout.new Qt::Vertical, self
 
-      # try to load config
-      
-      data = Plasma::ToolTipContent.new
-      data.mainText = "My Title"
-      data.subText = "My Title"
+      configChanged
 
-      button = Plasma::PushButton.new self
-      button.text = "Hi"
+      #data = Plasma::ToolTipContent.new
+      #data.mainText = "My Title"
+      #data.subText = "My Title"
+      #button = Plasma::PushButton.new self
+      #button.text = "Hi"
 
-      #@label.text = titles[2]
-      parsexml
+        self.layout = @layout
 
-      read_config
-      puts @mensa
-
- 
-      
-      layout.add_item @label
-
-      layout.add_item button
-      
-      #layout.add_item web
-
-      self.layout = layout
- 
-      resize 125, 125
+     # resize 125, 125
     end
 
-    def read_config
-      config = self.config
-      configGroup = config.group('Happa')
-      @mensa = configGroup.readEntry('Mensa','true')
-      puts @mensa
-    end
 
+    
     def configChanged
-      puts "xxx"
-      readxml
+      config = self.config
+      configGroup = config.group('url')
+      # pozor na to aby tam byl nil, ne "nil":)
+      @url_to_show = configGroup.readEntry('imageUrl', 'nil')
+        
+      extension = @url_to_show.split('.').last
+
+       # puts @url_to_show
+        puts extension
+
+        case extension
+        when "xml"
+          show_xml
+        when "html"
+          show_html
+        else
+          show_html
+        end
+    end
+
+    def reset_layout
+      puts "ZACINAM SMYCKU"
+      while ((@child = @layout.itemAt(0)) != nil)
+
+        @layout.removeItem(@child)
+        puts @child
+        @child.dispose
+      end
+      puts "KONCIm SMYCKU"
+
     end
 
 
-    def parsexml
+    def show_xml
+      reset_layout
+
       # Web search for "madonna"
-      url = 'http://localhost:3000/pcfilmtydne'
+      #url = 'http://localhost:3000/pcfilmtydne'
       #url = 'http://api.search.yahoo.com/WebSearchService/V1/webSearch?appid=YahooDemo&query=madonna&results=2'
+        
 
       # get the XML data as a string
-      xml_data = Net::HTTP.get_response(URI.parse(url)).body
+      xml_data = Net::HTTP.get_response(URI.parse(@url_to_show)).body
 
       # extract event information
       doc = REXML::Document.new(xml_data)
-      labels = []
-      links = []
       doc.elements.each("document/*") do |element|
         case element.name
         when "label"
           @label = Plasma::Label.new self
           @label.text = element.text
-          layout.add_item @label
+          @layout.add_item @label
+          puts "X"
+          puts @label
         when "image"
           @image = Plasma::WebView.new()
           @image.setUrl(KDE::Url.new(element.text))
-          layout.add_item @image
+          @layout.add_item @image
+          puts "X"
+          puts @image
         when "video" # potrebujeme KDE 4.3!
           @video = Plasma::VideoWidget.new()
           @video.setUrl(KDE::Url.new(element.text))
-          layout.add_item @video
+          @layout.add_item @video
+          puts "X"
+          puts @video
         end
       end
-      #@delka =titles.length
-#      doc.elements.each('ResultSet/Result/item') do |ele|
-#         links << ele.text
-#      end
+      #  self.layout = @layout
+    end
 
-      # print all events
-#--------------------------------------------------
-#       titles.each_with_index do |title, idx|
-#          print "#{title} => #{links[idx]}\n"
-#       end
-#-------------------------------------------------- 
-      labels
+    def show_html
 
+      reset_layout
+
+      @web = Plasma::WebView.new()
+      @web.setUrl(KDE::Url.new(@url_to_show))
+      @layout.add_item @web
+      puts "X"
+      puts @web
+#      self.layout = @layout
+       
     end
 
   end
